9c71991cba4904df37d1c65ded9bc4829e89350d
Akimio521 <akimio2333@gmail.com>
2025-07-20 16:54:18 +0800

    perfect(ParseVersion): 优化版本号解析正则表达式并添加测试用例，修复过度匹配的问题

--- meta/utils.go
+++ meta/utils.go
@@ -7,13 +7,14 @@ import (
 
 var (
 	// 版本号识别正则
-	version1Re = regexp.MustCompile(`(\d{2})[vV](\d+)`)              // 匹配 03v2 这种格式
-	version2Re = regexp.MustCompile(`\[(\d{0,2}([AaBb]|[Vv]\d)?)\]`) // 匹配 [03v2] 或 [1A] 这种格式
-	version3Re = regexp.MustCompile(` \d{1,2}[AaBb] `)               // 匹配 1A 或 2B 这种格式
+	version1Re     = regexp.MustCompile(`(\d{1,2})[vV](\d+)`)            // 匹配 03v2 这种格式
+	version2Re     = regexp.MustCompile(`\[(\d{1,2}[AaBbVv]\d*)\]`)      // 匹配 [12A] 或 [03v2] 这种格式
+	version3Re     = regexp.MustCompile(`(\d{1,2}[AaBb])(?:\s|\]|\[|$)`) // 匹配 12A 或 12B 这种格式
+	versionDigitRe = regexp.MustCompile(`[AaBb]|[vV](\d+)`)              // 提取字母或v后的数字
 )
 
 // 解析版本号，返回 uint8 类型版本号（0-255）
-// 解析失败时返回 0
+// 解析失败时返回 1（默认版本）
 func ParseVersion(s string) uint8 {
 	// 尝试第1种版本格式 数字+v+数字 (如 03v2)
 	if matches := version1Re.FindStringSubmatch(s); len(matches) > 2 {
@@ -22,57 +23,39 @@ func ParseVersion(s string) uint8 {
 		}
 	}
 
-	// 尝试第2种版本格式 [数字][字母]
+	// 尝试第2种版本格式 [数字+字母] (如 [12A], [03v2])
 	if matches := version2Re.FindStringSubmatch(s); len(matches) > 1 {
 		versionStr := matches[1] // 提取括号内的版本部分
+		return parseVersionFromString(versionStr)
+	}
 
-		// 检查是否包含字母，如果包含则返回对应的版本号
-		for i, c := range versionStr {
-			switch c {
-			case 'A', 'a':
-				return 1
-			case 'B', 'b':
-				return 2
-			case 'V', 'v':
-				// 如果是V后面跟数字，提取V后面的数字
-				if i+1 < len(versionStr) {
-					numStr := versionStr[i+1:]
-					if num, err := strconv.ParseUint(numStr, 10, 8); err == nil {
-						return uint8(num)
-					}
-				}
-			}
-		}
+	// 尝试第3种版本格式 数字+字母 (如 12A, 12B)
+	if matches := version3Re.FindStringSubmatch(s); len(matches) > 1 {
+		versionStr := matches[1] // 提取版本部分
+		return parseVersionFromString(versionStr)
+	}
 
-		// 如果没有字母，尝试提取纯数字部分
-		numStr := ""
-		for _, c := range versionStr {
-			if c >= '0' && c <= '9' {
-				numStr += string(c)
-			}
-		}
+	// 所有格式都不匹配，返回默认版本1
+	return 1
+}
 
-		// 转换数字部分
-		if numStr != "" {
-			if num, err := strconv.ParseUint(numStr, 10, 8); err == nil {
+// 从版本字符串中解析版本号
+func parseVersionFromString(versionStr string) uint8 {
+	// 查找字母或v+数字的模式
+	if matches := versionDigitRe.FindStringSubmatch(versionStr); len(matches) > 0 {
+		match := matches[0]
+		switch {
+		case match == "A" || match == "a":
+			return 1
+		case match == "B" || match == "b":
+			return 2
+		case len(matches) > 1 && matches[1] != "": // v后面有数字
+			if num, err := strconv.ParseUint(matches[1], 10, 8); err == nil {
 				return uint8(num)
 			}
 		}
 	}
 
-	// 尝试第3种版本格式 数字+字母+空格
-	if matches := version3Re.FindString(s); matches != "" {
-		// 检查字母类型
-		for _, c := range matches {
-			switch c {
-			case 'A', 'a':
-				return 1
-			case 'B', 'b':
-				return 2
-			}
-		}
-	}
-
-	// 所有格式都不匹配
-	return 0
+	// 如果没有找到版本标识符，返回默认版本1
+	return 1
 }
--- meta/utils_test.go
+++ meta/utils_test.go
@@ -11,8 +11,8 @@ func TestParseVersion(t *testing.T) {
 		input    string
 		expected uint8
 	}{
-		{"[SweetSub&LoliHouse] Made in Abyss S2 - 03v2 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv", 2},
 		{"[HKGS]昨日之歌[12A][1080P][WEB-DL].mp4", 1},
+		{"[SweetSub&LoliHouse] Made in Abyss S2 - 03v2 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv", 2},
 		{"[ANi] 聖劍傳說 Legend of Mana - The Teardrop Crystal - - 12B [1080P][Baha][WEB-DL][AAC AVC][CHT].mkv", 2},
 		{"[Haruhana] Kaoru Hana wa Rin to Saku - 02v2 [WebRip][HEVC-10bit 1080p][CHI_JPN].mkv", 2},
 		{"[SweetSub&VCB-Studio] Boogiepop Phantom [02v2][Ma10p_720p][x265_flac].mkv", 2},
@@ -21,6 +21,10 @@ func TestParseVersion(t *testing.T) {
 		{"[ANi]異世界魔王與召喚少女的奴隸魔術 Ω[02v2][1080P][Baha][WEB-DL].mp4", 2},
 		{"[ANi]總之就是非常可愛[12v2][1080P][WEB-DL-B].mp4", 2},
 		{"[ANi] D4DJ All Mix - 02v2 [1080P][Baha][WEB-DL][AAC AVC][CHT].mkv", 2},
+
+		{"【爪爪字幕组】★7月新番[欢迎来到实力至上主义的教室 第二季/Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e S2][11][1080p][HEVC][GB][MP4][招募翻译校对]", 1},
+		{"[GM-Team][国漫][寻剑 第1季][Sword Quest Season 1][2022][02][AVC][GB][1080P]", 1},
+		{"[猎户不鸽发布组] 组长女儿与照料专员 / 组长女儿与保姆 Kumichou Musume to Sewagakari [09] [1080p+] [简中内嵌] [2022年7月番]", 1},
 	}
 
 	for _, test := range tests {
