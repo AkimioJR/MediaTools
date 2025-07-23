f640f976c3a0a2a9bed3e89c27339d5fcd0ec32b
Akimio521 <akimio2333@gmail.com>
2025-07-23 15:19:42 +0800

    feat(logging): 添加日志级别设置功能，支持从配置中读取日志级别

--- internal/logging/log.go
+++ internal/logging/log.go
@@ -1,6 +1,8 @@
 package logging
 
 import (
+	"MediaTools/internal/config"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -8,6 +10,19 @@ func init() {
 	setting := &serviceLoggerSetting{}
 	logrus.SetFormatter(setting)
 	logrus.AddHook(setting)
-	logrus.SetLevel(logrus.InfoLevel) // 默认日志级别为 Info
-	logrus.SetReportCaller(true)      // 启用调用者信息
+	logrus.SetReportCaller(true) // 启用调用者信息
+	err := SetLogLevel(config.Log.Level)
+	if err != nil {
+		logrus.Errorf("设置日志级别失败，已设置为「%s」: %v", logrus.InfoLevel, err)
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+}
+
+func SetLogLevel(level string) error {
+	logLevel, err := logrus.ParseLevel(level)
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(logLevel)
+	return nil
 }
