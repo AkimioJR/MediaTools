32419ffe3e5a08e54e46a4a3d22c264489c83c75
Akimio521 <akimio2333@gmail.com>
2025-07-24 22:11:44 +0800

    refactor(tmdb_controller): 统一函数命名

--- internal/controller/tmdb_controller/credit.go
+++ internal/controller/tmdb_controller/credit.go
@@ -2,18 +2,18 @@ package tmdb_controller
 
 import "MediaTools/internal/pkg/themoviedb/v3"
 
-func GetMovieCredits(movieID int, language *string) (*themoviedb.MovieCredit, error) {
+func GetMovieCredit(movieID int, language *string) (*themoviedb.MovieCredit, error) {
 	return client.GetMovieCredit(movieID, language)
 }
 
-func GetTVSeriesCredits(seriesID int, language *string) (*themoviedb.TVSerieCredit, error) {
+func GetTVSerieCredit(seriesID int, language *string) (*themoviedb.TVSerieCredit, error) {
 	return client.GetTVSerieCredit(seriesID, language)
 }
 
-func GetTVSeasonCredits(seriesID int, seasonNumber int, language *string) (*themoviedb.TVSeasonCredit, error) {
+func GetTVSeasonCredit(seriesID int, seasonNumber int, language *string) (*themoviedb.TVSeasonCredit, error) {
 	return client.GetTVSeasonCredit(seriesID, seasonNumber, language)
 }
 
-func GetTVEpisodeCredits(seriesID int, seasonNumber int, episodeNumber int, language *string) (*themoviedb.TVEpisodeCredit, error) {
+func GetTVEpisodeCredit(seriesID int, seasonNumber int, episodeNumber int, language *string) (*themoviedb.TVEpisodeCredit, error) {
 	return client.GetTVEpisodeCredit(seriesID, seasonNumber, episodeNumber, language)
 }
