3f7a6ce534caa32d8d34f2b507577885c43c2109
Akimio521 <akimio2333@gmail.com>
2025-07-28 18:18:40 +0800

    feat(tmdb_controller): 添加获取电影、电视剧、季和剧集图片的功能

--- internal/controller/tmdb_controller/img.go
+++ internal/controller/tmdb_controller/img.go
@@ -1,6 +1,9 @@
 package tmdb_controller
 
-import "image"
+import (
+	"MediaTools/internal/pkg/themoviedb/v3"
+	"image"
+)
 
 func GetImageURL(path string) string {
 	return client.GetImageURL(path)
@@ -8,4 +11,20 @@ func GetImageURL(path string) string {
 
 func DownloadImage(path string) (image.Image, error) {
 	return client.DownloadImage(path)
+}
+
+func GetMovieImage(movieID int) (*themoviedb.MovieImage, error) {
+	return client.GetMovieImage(movieID, nil, nil)
+}
+
+func GetTVSerieImage(tvID int) (*themoviedb.TVSerieImage, error) {
+	return client.GetTVSerieImage(tvID, nil, nil)
+}
+
+func GetTVSeasonImage(tvID, seasonNumber int) (*themoviedb.TVSeasonImage, error) {
+	return client.GetTVSeasonImage(tvID, seasonNumber, nil)
+}
+
+func GetTVEpisodeImage(tvID, seasonNumber, episodeNumber int) (*themoviedb.TVEpisodeImage, error) {
+	return client.GetTVEpisodeImage(tvID, seasonNumber, episodeNumber, nil)
 }
