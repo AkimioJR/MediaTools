c3be397d6f5b1ecca1df6393cebf56068efc963b
Akimio521 <akimio2333@gmail.com>
2025-07-29 10:32:04 +0800

    feat(fanart_controller): 在 Init 函数中支持自定义 API URL 和 HTTP 客户端

--- internal/controller/fanart_controller/init.go
+++ internal/controller/fanart_controller/init.go
@@ -2,6 +2,7 @@ package fanart_controller
 
 import (
 	"MediaTools/internal/config"
+	"MediaTools/internal/outbound"
 	"MediaTools/internal/pkg/fanart/v3"
 	"sync"
 )
@@ -15,7 +16,13 @@ func Init() error {
 	lock.Lock()
 	defer lock.Unlock()
 
-	c, err := fanart.NewClient(config.Fanart.ApiKey)
+	var opts []fanart.Options
+	if config.Fanart.ApiURL != "" {
+		opts = append(opts, fanart.CustomAPIURL(config.Fanart.ApiURL))
+	}
+	opts = append(opts, fanart.CustomHTTPClient(outbound.GetHTTPClient()))
+
+	c, err := fanart.NewClient(config.Fanart.ApiKey, opts...)
 	if err != nil {
 		return err
 	}
