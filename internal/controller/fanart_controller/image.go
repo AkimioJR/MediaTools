4c6821db53846054f4b12788cc601ae28e9c452e
Akimio521 <akimio2333@gmail.com>
2025-07-29 10:06:39 +0800

    refactor(fanart): 更新 SortByLanguages 函数，移除过滤逻辑

--- internal/controller/fanart_controller/image.go
+++ internal/controller/fanart_controller/image.go
@@ -3,7 +3,10 @@ package fanart_controller
 import (
 	"MediaTools/internal/config"
 	"MediaTools/internal/pkg/fanart/v3"
+	"encoding/json"
 	"image"
+
+	"github.com/sirupsen/logrus"
 )
 
 func GetMovieImagesData(imdbID string) (*fanart.MovieImagesData, error) {
@@ -15,14 +18,14 @@ func GetMovieImagesData(imdbID string) (*fanart.MovieImagesData, error) {
 		return nil, err
 	}
 
-	data.HDMovieLogo = fanart.SortByLanguages(data.HDMovieLogo, config.Fanart.Languages)
-	data.MovieLogo = fanart.SortByLanguages(data.MovieLogo, config.Fanart.Languages)
-	data.MoviePoster = fanart.SortByLanguages(data.MoviePoster, config.Fanart.Languages)
-	data.HDMovieClearArt = fanart.SortByLanguages(data.HDMovieClearArt, config.Fanart.Languages)
-	data.MovieArt = fanart.SortByLanguages(data.MovieArt, config.Fanart.Languages)
-	data.MovieBackground = fanart.SortByLanguages(data.MovieBackground, config.Fanart.Languages)
-	data.MovieBanner = fanart.SortByLanguages(data.MovieBanner, config.Fanart.Languages)
-	data.MovieThumb = fanart.SortByLanguages(data.MovieThumb, config.Fanart.Languages)
+	fanart.SortByLanguages(data.HDMovieLogo, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MovieLogo, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MoviePoster, config.Fanart.Languages)
+	fanart.SortByLanguages(data.HDMovieClearArt, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MovieArt, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MovieBackground, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MovieBanner, config.Fanart.Languages)
+	fanart.SortByLanguages(data.MovieThumb, config.Fanart.Languages)
 
 	return data, nil
 }
@@ -35,19 +38,23 @@ func GetTVImagesData(thetvdbID int) (*fanart.TVImagesData, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		d, _ := json.Marshal(data)
+		logrus.Debugf("获取 Fanart TV 数据: %s", string(d))
+	}()
 
-	data.ClearLogo = fanart.SortByLanguages(data.ClearLogo, config.Fanart.Languages)
-	data.HDTVLogo = fanart.SortByLanguages(data.HDTVLogo, config.Fanart.Languages)
-	data.ClearArt = fanart.SortByLanguages(data.ClearArt, config.Fanart.Languages)
-	data.ShowBackground = fanart.SortByLanguages(data.ShowBackground, config.Fanart.Languages)
-	data.TVThumb = fanart.SortByLanguages(data.TVThumb, config.Fanart.Languages)
-	data.SeasonPoster = fanart.SortByLanguages(data.SeasonPoster, config.Fanart.Languages)
-	data.SeasonThumb = fanart.SortByLanguages(data.SeasonThumb, config.Fanart.Languages)
-	data.HDClearArt = fanart.SortByLanguages(data.HDClearArt, config.Fanart.Languages)
-	data.TVBanner = fanart.SortByLanguages(data.TVBanner, config.Fanart.Languages)
-	data.CharacterArt = fanart.SortByLanguages(data.CharacterArt, config.Fanart.Languages)
-	data.TVPoster = fanart.SortByLanguages(data.TVPoster, config.Fanart.Languages)
-	data.SeasonBanner = fanart.SortByLanguages(data.SeasonBanner, config.Fanart.Languages)
+	fanart.SortByLanguages(data.ClearLogo, config.Fanart.Languages)
+	fanart.SortByLanguages(data.HDTVLogo, config.Fanart.Languages)
+	fanart.SortByLanguages(data.ClearArt, config.Fanart.Languages)
+	fanart.SortByLanguages(data.ShowBackground, config.Fanart.Languages)
+	fanart.SortByLanguages(data.TVThumb, config.Fanart.Languages)
+	fanart.SortByLanguages(data.SeasonPoster, config.Fanart.Languages)
+	fanart.SortByLanguages(data.SeasonThumb, config.Fanart.Languages)
+	fanart.SortByLanguages(data.HDClearArt, config.Fanart.Languages)
+	fanart.SortByLanguages(data.TVBanner, config.Fanart.Languages)
+	fanart.SortByLanguages(data.CharacterArt, config.Fanart.Languages)
+	fanart.SortByLanguages(data.TVPoster, config.Fanart.Languages)
+	fanart.SortByLanguages(data.SeasonBanner, config.Fanart.Languages)
 
 	return data, nil
 
--- internal/pkg/fanart/v3/utils.go
+++ internal/pkg/fanart/v3/utils.go
@@ -5,22 +5,9 @@ import (
 	"sort"
 )
 
-// 根据 languages 优先级排序并过滤 images，只有 Lang 在 languages 中的才保留
-func SortByLanguages[T img](images []T, languages []string) []T {
-	result := make([]T, 0, len(images))
-
-	// 过滤不在 langs 中的图片
-	for _, img := range images {
-		if !slices.Contains(languages, img.getLang()) {
-			continue
-		}
-		result = append(result, img)
-	}
-
-	// 根据 langs 的顺序排序
-	sort.Slice(result, func(i, j int) bool {
-		return slices.Index(languages, result[i].getLang()) < slices.Index(languages, result[j].getLang())
+// 根据 languages 优先级排序 BaseInfo
+func SortByLanguages[T img](infos []T, languages []string) {
+	sort.Slice(infos, func(i, j int) bool {
+		return slices.Index(languages, infos[i].getLang()) < slices.Index(languages, infos[j].getLang())
 	})
-
-	return result
 }
