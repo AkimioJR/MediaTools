ebfec6108c3f579f4d632215ec78a5174431a85e
Akimio521 <akimio2333@gmail.com>
2025-07-29 11:07:13 +0800

    feat(extensions): 添加字幕、音轨和音频文件扩展名

--- extensions/media.go
+++ extensions/media.go
@@ -23,3 +23,24 @@ var MediaExtensions = []string{
 
 // MediaExtensionsExtended 包含所有媒体扩展名以及额外的 .strm 扩展名
 var MediaExtensionsExtended = append(MediaExtensions, ".strm")
+
+// 字幕文件扩展名
+var SubtitleExtensions = []string{
+	".srt", ".ass", ".ssa", ".sup",
+}
+
+// 音轨文件扩展名
+var AudioTrackExtensions = []string{
+	".mka",
+}
+
+// 音频文件扩展名
+var AudioExtensions = []string{
+	".aac", ".ac3", ".amr", ".caf", ".cda", ".dsf",
+	".dff", ".kar", ".m4a", ".mp1", ".mp2", ".mp3",
+	".mid", ".mod", ".mka", ".mpc", ".nsf", ".ogg",
+	".pcm", ".rmi", ".s3m", ".snd", ".spx", ".tak",
+	".tta", ".vqf", ".wav", ".wma",
+	".aifc", ".aiff", ".alac", ".adif", ".adts",
+	".flac", ".midi", ".opus", ".sfalc",
+}
