a59c61ef2b9b9d5072e60ffbaa85bb31a470976e
Akimio521 <akimio2333@gmail.com>
2025-07-29 10:36:05 +0800

    feat(controller): 添加初始化所有控制器的功能

---
+++ internal/controller/init.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"MediaTools/internal/controller/fanart_controller"
+	"MediaTools/internal/controller/scrape_controller"
+	"MediaTools/internal/controller/tmdb_controller"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type InitFunc func() error
+
+var initFuncs = []InitFunc{
+	tmdb_controller.Init,
+	fanart_controller.Init,
+	scrape_controller.Init,
+}
+
+func InitAllControllers() error {
+	logrus.Info("开始初始化全部工具链...")
+	for _, initFunc := range initFuncs {
+		if err := initFunc(); err != nil {
+			return fmt.Errorf("初始化工具链失败: %w", err)
+		}
+	}
+	logrus.Info("全部工具链初始化完成")
+	return nil
+}
