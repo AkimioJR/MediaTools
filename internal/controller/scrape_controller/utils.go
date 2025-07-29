10d73a2b54a9565cc9aa3f1b6464cbbe43e09af4
Akimio521 <akimio2333@gmail.com>
2025-07-29 10:06:59 +0800

    feat(scrape_controller): 添加 ChangeExt 和 CreateFile 函数以支持文件扩展名更改和文件创建

--- internal/controller/scrape_controller/utils.go
+++ internal/controller/scrape_controller/utils.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 )
 
 // 保存为 JPEG
@@ -77,4 +78,18 @@ func DownloadFanartImageAndSave(url string, target string) error {
 		return err
 	}
 	return SaveImage(target, img)
+}
+
+func ChangeExt(filename, newExt string) string {
+	base := filepath.Base(filename)
+	ext := filepath.Ext(base)
+	return strings.TrimSuffix(filename, ext) + newExt
+}
+
+func CreateFile(filePath string) (*os.File, error) {
+	err := os.MkdirAll(filepath.Dir(filePath), 0755)
+	if err != nil {
+		return nil, err
+	}
+	return os.Create(filePath)
 }
