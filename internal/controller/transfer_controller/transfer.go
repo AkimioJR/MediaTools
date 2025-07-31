61b1e14423db72e26be2d15543db740d6bc7669e
Akimio521 <akimio2333@gmail.com>
2025-07-31 10:23:42 +0800

    refactor(transfer_controller): 重构 TransferMedia 函数，使用 FileInfo 结构体替代字符串路径，并移除 utils.go 文件

--- internal/controller/transfer_controller/transfer.go
+++ internal/controller/transfer_controller/transfer.go
@@ -3,9 +3,9 @@ package transfer_controller
 import (
 	"MediaTools/extensions"
 	"MediaTools/internal/controller/scrape_controller"
+	"MediaTools/internal/controller/storage_controller"
 	"MediaTools/internal/schemas"
 	"MediaTools/utils"
-	"os"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -14,14 +14,14 @@ import (
 )
 
 // 识别并整理一个文件
-// srcPath: 源文件路径
-// targetDir: 目标目录
+// srcFile: 源文件
+// dstDir: 目标目录
 // transferType: 传输类型（复制、移动、链接等）
 // item: 媒体项（包含元数据）
 // info: 识别到的媒体信息（为nil代表无需刮削）
 func TransferMedia(
-	srcPath string,
-	targetDir string,
+	srcFile *schemas.FileInfo,
+	dstDir *schemas.FileInfo,
 	transferType schemas.TransferType,
 	item *schemas.MediaItem,
 	info *schemas.MediaInfo,
@@ -33,42 +33,48 @@ func TransferMedia(
 	if err != nil {
 		return err
 	}
-	targetPath := filepath.Join(targetDir, targetName)
-	logrus.Infof("开始转移媒体文件：%s -> %s，类型：%s", srcPath, targetPath, transferType)
+	dstFile := storage_controller.Join(dstDir, targetName)
+	logrus.Infof("开始转移媒体文件：%s -> %s，转移类型类型：%s", srcFile, dstFile, transferType)
 
-	err = TransferFile(srcPath, targetPath, transferType)
+	err = storage_controller.TransferFile(srcFile, dstFile, transferType)
 	if err != nil {
 		return err
 	}
 
 	{
 		logrus.Info("开始转移字幕/音轨文件")
-		srcDir := filepath.Dir(srcPath)
-		entries, err := os.ReadDir(srcDir)
+		srcDir := storage_controller.GetParent(srcFile)
+		fileInfos, err := storage_controller.List(srcDir)
 		if err != nil {
 			logrus.Warningf("读取目录失败，跳过转移字幕/音轨文件：%v", err)
-		}
-		exts := append(extensions.SubtitleExtensions, extensions.AudioTrackExtensions...)
-		for _, entry := range entries {
-			if entry.IsDir() || entry.Name() == filepath.Base(srcPath) {
-				continue // 跳过目录和源文件本身
-			}
+		} else {
+			exts := append(extensions.SubtitleExtensions, extensions.AudioTrackExtensions...)
+			for _, fi := range fileInfos {
+				if fi.IsDir || fi.Path == srcDir.Path {
+					continue // 跳过目录和源文件本身
+				}
 
-			if slices.Contains(exts, strings.ToLower(filepath.Ext(entry.Name()))) {
-				srcFilePath := filepath.Join(srcDir, entry.Name())
-				dstFilePath := utils.ChangeExt(targetPath, filepath.Ext(entry.Name()))
-				logrus.Debugf("转移字幕/音轨文件：%s -> %s", srcFilePath, dstFilePath)
-				err = CopyFile(srcFilePath, dstFilePath) // 复制字幕/音轨文件
-				if err != nil {
-					logrus.Warningf("转移字幕/音轨文件失败：%v", err)
+				if slices.Contains(exts, strings.ToLower(filepath.Ext(fi.Name))) {
+					otherDstFilePath := utils.ChangeExt(dstFile.Path, filepath.Ext(fi.Name))
+					otherDstFile, err := storage_controller.GetFile(otherDstFilePath, dstFile.StorageType)
+					if err != nil {
+						logrus.Warningf("获取文件 %s:%s 失败: %v", dstFile.StorageType, otherDstFilePath, err)
+						continue
+					}
+					logrus.Debugf("转移字幕/音轨文件：%s -> %s", fi.String(), otherDstFile)
+					err = storage_controller.Copy(&fi, otherDstFile) // 复制字幕/音轨文件
+					if err != nil {
+						logrus.Warningf("转移字幕/音轨文件失败：%v", err)
+					}
 				}
 			}
+
 		}
 	}
 
 	if info != nil {
 		logrus.Info("开始生成刮削元数据")
-		err = scrape_controller.Scrape(targetPath, info)
+		err = scrape_controller.Scrape(dstFile, info)
 		if err != nil {
 			logrus.Warningf("刮削数据失败：%v", err)
 		}
