bac776474501a657eabb1a2074f158ac9ef3fabd
Akimio521 <akimio2333@gmail.com>
2025-08-07 10:16:43 +0800

    fix(config): 将 YAML 标签更改为 JSON 标签以支持 JSON 序列化

--- internal/config/type.go
+++ internal/config/type.go
@@ -3,54 +3,54 @@ package config
 import "MediaTools/internal/schemas"
 
 type LogConfig struct {
-	Level string `yaml:"level"` // 日志级别
-	Path  string `yaml:"path"`  // 日志文件目录
+	Level string `json:"level"` // 日志级别
+	Path  string `json:"path"`  // 日志文件目录
 }
 
 type TMDBConfig struct {
-	ApiURL   string `yaml:"api_url"`
-	ImageURL string `yaml:"image_url"`
-	ApiKey   string `yaml:"api_key"`
-	Language string `yaml:"language"`
+	ApiURL   string `json:"api_url"`
+	ImageURL string `json:"image_url"`
+	ApiKey   string `json:"api_key"`
+	Language string `json:"language"`
 }
 
 type FanartConfig struct {
-	ApiKey    string   `yaml:"api_key"`
-	ApiURL    string   `yaml:"api_url"`
-	Languages []string `yaml:"languages"` // 语言顺序
+	ApiKey    string   `json:"api_key"`
+	ApiURL    string   `json:"api_url"`
+	Languages []string `json:"languages"` // 语言顺序
 }
 
 type Configuration struct {
-	Log        LogConfig        `yaml:"log"`
-	TMDB       TMDBConfig       `yaml:"tmdb"`
-	Fanart     FanartConfig     `yaml:"fanart"`
-	Media      MediaConfig      `yaml:"media"`
-	CustomWord CustomWordConfig `yaml:"custom_word"`
+	Log        LogConfig        `json:"log"`
+	TMDB       TMDBConfig       `json:"tmdb"`
+	Fanart     FanartConfig     `json:"fanart"`
+	Media      MediaConfig      `json:"media"`
+	CustomWord CustomWordConfig `json:"custom_word"`
 }
 
 type MediaConfig struct {
-	Libraries []LibraryConfig `yaml:"libraries"` // 媒体库路径列表
-	Format    FormatConfig    `yaml:"format"`    // 媒体格式配置
+	Libraries []LibraryConfig `json:"libraries"` // 媒体库路径列表
+	Format    FormatConfig    `json:"format"`    // 媒体格式配置
 }
 
 type FormatConfig struct {
-	Movie string `yaml:"movie"` // 电影格式
-	TV    string `yaml:"tv"`    // 电视剧格式
+	Movie string `json:"movie"` // 电影格式
+	TV    string `json:"tv"`    // 电视剧格式
 }
 
 type LibraryConfig struct {
-	Name               string               `yaml:"name"`                 // 媒体库名称
-	SrcPath            string               `yaml:"src_path"`             // 源路径
-	SrcType            schemas.StorageType  `yaml:"src_type"`             // 源类型
-	DstType            schemas.StorageType  `yaml:"dst_type"`             // 目标类型
-	DstPath            string               `yaml:"dst_path"`             // 目标路径
-	TransferType       schemas.TransferType `yaml:"transfer_type"`        // 传输类型
-	OrganizeByType     bool                 `yaml:"organize_by_type"`     // 是否按类型分文件夹
-	OrganizeByCategory bool                 `yaml:"organize_by_category"` // 是否按分类分文件夹
+	Name               string               `json:"name"`                 // 媒体库名称
+	SrcPath            string               `json:"src_path"`             // 源路径
+	SrcType            schemas.StorageType  `json:"src_type"`             // 源类型
+	DstType            schemas.StorageType  `json:"dst_type"`             // 目标类型
+	DstPath            string               `json:"dst_path"`             // 目标路径
+	TransferType       schemas.TransferType `json:"transfer_type"`        // 传输类型
+	OrganizeByType     bool                 `json:"organize_by_type"`     // 是否按类型分文件夹
+	OrganizeByCategory bool                 `json:"organize_by_category"` // 是否按分类分文件夹
 }
 
 type CustomWordConfig struct {
-	IdentifyWord  []string `yaml:"identify_word"` // 自定义识别词
-	Customization []string `yaml:"customization"` // 自定义占位置词
-	ExcludeWords  []string `yaml:"exclude_words"` // 自定义排除词
+	IdentifyWord  []string `json:"identify_word"` // 自定义识别词
+	Customization []string `json:"customization"` // 自定义占位置词
+	ExcludeWords  []string `json:"exclude_words"` // 自定义排除词
 }
