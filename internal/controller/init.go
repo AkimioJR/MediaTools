c6a6bd861b17e5e6fc54e3ae79dd7c2f8514715d
Akimio521 <akimio2333@gmail.com>
2025-08-11 18:07:04 +0800

    feat: 移除初始化工具链时的日志记录

--- internal/controller/init.go
+++ internal/controller/init.go
@@ -8,8 +8,6 @@ import (
 	"MediaTools/internal/controller/storage_controller"
 	"MediaTools/internal/controller/tmdb_controller"
 	"fmt"
-
-	"github.com/sirupsen/logrus"
 )
 
 type InitFunc func() error
@@ -24,12 +22,10 @@ var initFuncs = []InitFunc{
 }
 
 func InitAllControllers() error {
-	logrus.Info("开始初始化全部工具链...")
 	for _, initFunc := range initFuncs {
 		if err := initFunc(); err != nil {
 			return fmt.Errorf("初始化工具链失败: %w", err)
 		}
 	}
-	logrus.Info("全部工具链初始化完成")
 	return nil
 }
