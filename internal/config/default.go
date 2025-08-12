86d29095c86de8b336938473b9883ff4ed1e9177
Akimio521 <akimio2333@gmail.com>
2025-08-12 09:16:42 +0800

    feat: 默认配置添加媒体格式配置

--- internal/config/default.go
+++ internal/config/default.go
@@ -16,4 +16,10 @@ var defaultConfig = Configuration{
 		ApiKey: "YOUR_FANART_API_KEY", // 请替换为您的 Fanart API Key
 		ApiURL: "https://webservice.fanart.tv",
 	},
+	Media: MediaConfig{
+		Format: FormatConfig{
+			Movie: "{{.Title}} ({{.Year}})/{{.Title}} ({{.Year}}){{if .Part}} -{{.Part}}{{end}}{{if .Version}} -v{{.Version}}{{end}}{{if .ReleaseGroups}} -{{end}}{{range .ReleaseGroups}}@{{.}}{{end}}{{if .ResourcePix}} -{{.ResourcePix}}{{end}}{{if .ResourceType}} -{{.ResourceType}}{{end}}{{if .ResourceEffect}} -{{end}}{{range .ResourceEffect}}@{{.}}{{end}}{{if .Platform}} -{{.Platform}}{{end}}{{if .VideoEncode}} -{{.VideoEncode}}{{end}}{{if .AudioEncode}} -{{.AudioEncode}}{{end}}{{.FileExtension}}",
+			TV:    "{{.Title}} ({{.Year}})/Season {{.Season}}/{{.Title}} {{.SeasonStr}}{{.EpisodeStr}}{{if .EpisodeTitle}} {{.EpisodeTitle}}{{end}}{{if .Part}} -{{.Part}}{{end}}{{if .Version}} -v{{.Version}}{{end}}{{if .ReleaseGroups}} -{{end}}{{range .ReleaseGroups}}@{{.}}{{end}}{{if .ResourcePix}} -{{.ResourcePix}}{{end}}{{if .ResourceType}} -{{.ResourceType}}{{end}}{{if .ResourceEffect}} -{{end}}{{range .ResourceEffect}}@{{.}}{{end}}{{if .Platform}} -{{.Platform}}{{end}}{{if .VideoEncode}} -{{.VideoEncode}}{{end}}{{if .AudioEncode}} -{{.AudioEncode}}{{end}}{{.FileExtension}}",
+		},
+	},
 }
