c799a45ea9d7b297b0b385b47b5d77c409c243e0
Akimio521 <akimio2333@gmail.com>
2025-08-12 15:59:46 +0800

    refactor(FileLogsHook): 重构日志钩子，优化异步写入逻辑避免并发性问题并确保资源正确释放

--- internal/pkg/loghook/file.go
+++ internal/pkg/loghook/file.go
@@ -1,8 +1,10 @@
 package loghook
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -10,11 +12,61 @@ import (
 
 type FileLogsHook struct {
 	LogDir string
+	file   *os.File
+	day    int                // 用于跟踪当前日志文件的日期
+	path   string             // 用于跟踪当前日志文件路径
+	ch     chan *logrus.Entry // 用于异步写入日志
+	wg     sync.WaitGroup     // 用于等待写入协程结束
 }
 
 func NewFileLogsHook(logDir string) *FileLogsHook {
-	return &FileLogsHook{
+	fh := FileLogsHook{
 		LogDir: logDir,
+		ch:     make(chan *logrus.Entry, 500), // 缓冲通道，用于异步写入日志
+	}
+	go fh.writeLog() // 启动日志写入协程
+	return &fh
+}
+
+// Close 方法用于关闭文件和通道，确保资源被正确释放，避免协程泄漏
+func (f *FileLogsHook) Close() {
+	close(f.ch)
+	f.wg.Wait() // 等待写入协程完成
+	if f.file != nil {
+		f.file.Close()
+	}
+}
+
+// writeLog 是一个协程，用于异步写入日志到文件，避免并发写入问题
+func (f *FileLogsHook) writeLog() {
+	for entry := range f.ch {
+		f.wg.Add(1) // 增加等待组计数器
+		if f.day != entry.Time.Day() || f.file == nil {
+			if f.file != nil {
+				f.file.Close()
+			}
+			f.day = entry.Time.Day()
+			f.path = filepath.Join(f.LogDir, entry.Time.Local().Format("2006-01-02")+".log")
+			var err error
+			f.file, err = os.OpenFile(f.path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "open log file '%s' failed: %v", f.path, err)
+				f.wg.Done() // 结束当前日志条目的等待
+				continue
+			}
+			formatter := &logrus.JSONFormatter{
+				TimestampFormat: time.DateTime,
+			}
+
+			logData, err := formatter.Format(entry)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "format log entry failed: %v", err)
+				f.wg.Done() // 结束当前日志条目的等待
+				continue
+			}
+			f.file.Write(logData)
+			f.wg.Done() // 结束当前日志条目的等待
+		}
 	}
 }
 
@@ -23,26 +75,7 @@ func (f *FileLogsHook) Levels() []logrus.Level {
 }
 
 func (f *FileLogsHook) Fire(entry *logrus.Entry) error {
-	if err := os.MkdirAll(f.LogDir, os.ModePerm); err != nil {
-		return err
-	}
-	logFilePath := filepath.Join(f.LogDir, time.Now().Local().Format("2006-01-02")+".log")
-	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-	defer logFile.Close()
-
-	formatter := &logrus.JSONFormatter{
-		TimestampFormat: time.DateTime,
-	}
-
-	logData, err := formatter.Format(entry)
-	if err != nil {
-		return err
-	}
-
-	logFile.Write(logData)
+	f.ch <- entry // 将日志条目发送到通道
 	return nil
 }
 
