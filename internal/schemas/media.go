e6ed400ef31d0019514f8f94f649d2c2e67cf474
Akimio521 <akimio2333@gmail.com>
2025-08-14 21:33:47 +0800

    refactor: 更新识别媒体响应结构体，替换为 RecognizeMediaDetail，并移除旧的 RecognizeMediaResponse

--- internal/schemas/media.go
+++ internal/schemas/media.go
@@ -124,3 +124,9 @@ func NewMediaItem(videoMeta *meta.VideoMeta, info *MediaInfo) (*MediaItem, error
 
 	return &item, nil
 }
+
+type RecognizeMediaDetail struct {
+	Item       *MediaItem `json:"item"`        // 识别到的媒体项
+	CustomRule string     `json:"custom_rule"` // 应用自定义规则
+	MetaRule   string     `json:"meta_rule"`   // 应用的媒体规则
+}
--- internal/schemas/response.go
+++ internal/schemas/response.go
@@ -6,8 +6,3 @@ type ErrResponse struct {
 }
 
 // 识别视频媒体信息的响应结构体
-type RecognizeMediaResponse struct {
-	Item       *MediaItem `json:"item"`        // 识别到的媒体项
-	CustomRule string     `json:"custom_rule"` // 应用自定义规则
-	MetaRule   string     `json:"meta_rule"`   // 应用的媒体规则
-}
