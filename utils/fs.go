10be8d69dd2c923d33bf1953e33faa2074238b63
Akimio521 <akimio2333@gmail.com>
2025-08-16 23:16:25 +0800

    refactor: 移除未使用的 os 导入，清理代码

--- utils/fs.go
+++ utils/fs.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -10,12 +9,4 @@ func ChangeExt(filename, newExt string) string {
 	base := filepath.Base(filename)
 	ext := filepath.Ext(base)
 	return strings.TrimSuffix(filename, ext) + newExt
-}
-
-func CreateFile(filePath string) (*os.File, error) {
-	err := os.MkdirAll(filepath.Dir(filePath), 0755)
-	if err != nil {
-		return nil, err
-	}
-	return os.Create(filePath)
 }
