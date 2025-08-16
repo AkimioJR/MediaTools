988afc4f617779ace8f2c4900439610a9a65ab0f
Akimio521 <akimio2333@gmail.com>
2025-08-17 07:45:02 +0800

    fix: 更新 StorageFileInfo 结构体的 JSON 标签，确保字段正确序列化

--- internal/schemas/storage/file.go
+++ internal/schemas/storage/file.go
@@ -28,13 +28,13 @@ func NewStoragePath(storageType StorageType, path string) StoragePath {
 }
 
 type StorageFileInfo struct {
-	StorageType StorageType `json:"storage_type"` // 存储系统类型
-	Path        string      `json:"path"`         // 文件路径
-	Name        string      `json:"name"`         // 文件名
-	Ext         string      `json:"ext"`          // 文件扩展名
-	Size        int64       `json:"size"`         // 文件大小
-	IsDir       bool        `json:"is_dir"`       // 是否为目录
-	ModTime     time.Time   `json:"mod_time"`     // 文件修改时间
+	StorageType StorageType `json:"storage_type"`       // 存储系统类型
+	Path        string      `json:"path"`               // 文件路径
+	Name        string      `json:"name"`               // 文件名
+	Ext         string      `json:"ext"`                // 文件扩展名
+	Size        int64       `json:"size,omitempty"`     // 文件大小
+	IsDir       bool        `json:"is_dir,omitempty"`   // 是否为目录
+	ModTime     time.Time   `json:"mod_time,omitempty"` // 文件修改时间
 }
 
 func NewFileInfo(storageType StorageType, path string, size int64, isDir bool, modTime time.Time) *StorageFileInfo {
