aaa454eabc70a3f11da6bc0fc7280cd0865e0dd3
Akimio521 <akimio2333@gmail.com>
2025-08-17 21:33:11 +0800

    update: 移除底层归档 API

--- internal/router/library/init.go
+++ internal/router/library/init.go
@@ -3,8 +3,8 @@ package library
 import "github.com/gin-gonic/gin"
 
 func RegisterLibraryRouter(router *gin.Engine) {
-	libraryRouter := router.Group("/library") // 媒体库相关接口
-	{
-		libraryRouter.POST("/archive", ArchiveMedia) // 归档媒体文件
-	}
+	// libraryRouter := router.Group("/library") // 媒体库相关接口
+	// {
+	// 	libraryRouter.POST("/archive", ArchiveMedia) // 归档媒体文件
+	// }
 }
