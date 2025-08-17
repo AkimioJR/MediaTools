ba1cdc0dc2c787fe0e132d4696dce0e31169374f
Akimio521 <akimio2333@gmail.com>
2025-08-17 21:33:22 +0800

    feat: 添加解析集数或范围的函数 parseEpisodeStr

--- internal/controller/library_controller/utils.go
+++ internal/controller/library_controller/utils.go
@@ -5,7 +5,9 @@ import (
 	"MediaTools/internal/pkg/meta"
 	"MediaTools/internal/schemas"
 	"MediaTools/internal/schemas/storage"
+	"fmt"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -172,4 +174,31 @@ func GenFloder(libConfig *config.LibraryConfig, info *schemas.MediaInfo) []strin
 	}
 
 	return floderNames
+}
+
+// 支持解析集数或范围，
+// 例如 1 ---> 第1集
+// 例如 1,3 ---> 第1集到第3集
+func parseEpisodeStr(episodeStr string) (int, int, error) {
+	if strings.Contains(episodeStr, ",") { // 多集或范围
+		parts := strings.Split(episodeStr, ",")
+		if len(parts) != 2 {
+			return 0, 0, fmt.Errorf("集数格式错误，应该是单集或两集范围，输入字符串: %s", episodeStr)
+		}
+		start, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return 0, 0, fmt.Errorf("解析起始集数失败：%w", err)
+		}
+		end, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return 0, 0, fmt.Errorf("解析结束集数失败：%w", err)
+		}
+		return start, end, nil
+	} else { // 单集
+		episode, err := strconv.Atoi(episodeStr)
+		if err != nil {
+			return 0, 0, fmt.Errorf("解析集数失败：%w", err)
+		}
+		return episode, 0, nil
+	}
 }
