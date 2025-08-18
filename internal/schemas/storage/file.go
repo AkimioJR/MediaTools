c98a5532370cbbccab64655c1d412da3fe2146d4
Akimio521 <akimio2333@gmail.com>
2025-08-18 09:13:35 +0800

    feat: 添加 StorageEntry 接口及相关方法以增强存储条目管理

--- internal/schemas/storage/file.go
+++ internal/schemas/storage/file.go
@@ -28,20 +28,35 @@ func NewStoragePath(storageType StorageType, path string) StoragePath {
 	}
 }
 
+type StorageEntry interface {
+	StoragePath
+	GetFileType() FileType
+}
+
+func NewStorageEntry(storageType StorageType, path string, ft FileType) StorageEntry {
+	fi := NewStoragePath(storageType, path).(*StorageFileInfo)
+	fi.Type = ft
+	return fi
+}
+
 type StorageFileInfo struct {
-	StorageType StorageType `json:"storage_type"`      // 存储系统类型
-	Path        string      `json:"path"`              // 文件路径
-	Name        string      `json:"name"`              // 文件名
-	Ext         string      `json:"ext"`               // 文件扩展名
-	Size        int64       `json:"size,omitzero"`     // 文件大小
-	Type        FileType    `json:"type,omitzero"`     // 文件类型
-	ModTime     time.Time   `json:"mod_time,omitzero"` // 文件修改时间
+	// 基础路径信息 StoragePath
+	StorageType StorageType `json:"storage_type"` // 存储系统类型
+	Path        string      `json:"path"`         // 文件路径
+	Name        string      `json:"name"`         // 文件名
+	Ext         string      `json:"ext"`          // 文件扩展名
+
+	// 路径类型信息 StorageEntry
+	Type FileType `json:"type,omitzero"` // 文件类型
+
+	// 详细信息 StorageFileInfo
+	Size    int64     `json:"size,omitzero"`     // 文件大小
+	ModTime time.Time `json:"mod_time,omitzero"` // 文件修改时间
 }
 
 func NewFileInfo(storageType StorageType, path string, size int64, ft FileType, modTime time.Time) *StorageFileInfo {
-	fi := NewStoragePath(storageType, path).(*StorageFileInfo)
+	fi := NewStorageEntry(storageType, path, ft).(*StorageFileInfo)
 	fi.Size = size
-	fi.Type = ft
 	fi.ModTime = modTime
 	return fi
 }
@@ -77,8 +92,13 @@ func (fi *StorageFileInfo) LowerExt() string {
 	return strings.ToLower(fi.Ext)
 }
 
+func (fi *StorageFileInfo) GetFileType() FileType {
+	return fi.Type
+}
+
 func (fi *StorageFileInfo) String() string {
 	return fi.StorageType.String() + ":" + fi.Path
 }
 
 var _ StoragePath = (*StorageFileInfo)(nil)
+var _ StorageEntry = (*StorageFileInfo)(nil)
