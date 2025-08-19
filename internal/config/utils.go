4e58217819d0db5cf98b1c6b5a9977a203d08361
Akimio521 <akimio2333@gmail.com>
2025-08-19 11:17:38 +0800

    feat: 更新配置检查逻辑，添加缺失配置时自动保存配置文件

--- internal/config/utils.go
+++ internal/config/utils.go
@@ -61,51 +61,70 @@ var dbOnce sync.Once
 
 // 检查配置的完整性
 func (c *Configuration) check() {
+	needSave := false
 	dbOnce.Do(func() {
 		if c.DB.Type == model.DBTypeUnknown || c.DB.DSN == "" {
 			logrus.Warning("数据库类型或连接字符串未设置，使用默认 SQLite 配置")
 			c.DB.Type = model.DBTypeSQLite
 			c.DB.DSN = SQLiteDBFile
+			needSave = true
 		}
 	})
 
 	if c.Log.ConsoleLevel == "" {
 		logrus.Warning("日志终端输出级别未设置，使用默认配置")
 		c.Log.ConsoleLevel = defaultConfig.Log.ConsoleLevel
+		needSave = true
 	}
 
 	if c.Log.FileLevel == "" {
 		logrus.Warning("日志文件级别未设置，使用默认配置")
 		c.Log.FileLevel = defaultConfig.Log.FileLevel
+		needSave = true
 	}
 
 	if c.Log.FileDir == "" {
 		logrus.Warning("日志文件目录未设置，使用默认配置")
 		c.Log.FileDir = defaultConfig.Log.FileDir
+		needSave = true
 	}
 
 	if c.TMDB.ApiURL == "" {
 		logrus.Warning("TMDB API URL 配置未设置，使用默认配置")
 		c.TMDB.ApiURL = defaultConfig.TMDB.ApiURL
+		needSave = true
 	}
 
 	if c.TMDB.ImageURL == "" {
 		logrus.Warning("TMDB 图片 API URL 配置未设置，使用默认配置")
 		c.TMDB.ImageURL = defaultConfig.TMDB.ImageURL
+		needSave = true
 	}
 
 	if c.Fanart.ApiURL == "" {
 		logrus.Warning("Fanart API URL 配置未设置，使用默认配置")
 		c.Fanart.ApiURL = defaultConfig.Fanart.ApiURL
+		needSave = true
 	}
 
 	if len(c.Storages) == 0 {
 		logrus.Warning("存储配置未设置，使用默认配置")
 		c.Storages = defaultConfig.Storages
+		needSave = true
 	}
 
 	if c.Media.Format == (FormatConfig{}) {
 		logrus.Warning("媒体格式配置未设置，使用默认配置")
 		c.Media.Format = defaultConfig.Media.Format
+		needSave = true
+	}
+
+	if needSave {
+		logrus.Info("需要更新配置文件")
+		if err := c.writeConfig(); err != nil {
+			logrus.Errorf("保存配置文件失败: %v", err)
+		} else {
+			logrus.Info("配置文件已更新")
+		}
 	}
 }
