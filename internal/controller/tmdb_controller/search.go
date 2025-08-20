cefa3ffa2fb9d4fdd84b6bbcdf71614523263454
Akimio521 <akimio2333@gmail.com>
2025-08-20 12:26:04 +0800

    feat(tmdb_controller): 优化搜索功能，使用迭代器处理搜索结果

--- internal/controller/tmdb_controller/search.go
+++ internal/controller/tmdb_controller/search.go
@@ -6,6 +6,7 @@ import (
 	"MediaTools/internal/schemas"
 	"MediaTools/utils"
 	"fmt"
+	"iter"
 
 	"github.com/sirupsen/logrus"
 )
@@ -15,39 +16,59 @@ func SearchMovie(searchName string) (*schemas.MediaInfo, error) {
 	defer lock.RUnlock()
 
 	logrus.Infof("正在搜索「%s」...", searchName)
-	var page uint32 = 1
-	var params = themoviedb.SearchMovieParams{
-		Query: searchName,
-		Page:  &page,
-	}
 
-	resp, err := client.SearchMovie(params)
-	if err != nil {
-		return nil, fmt.Errorf("搜索电影「%s」失败: %v", searchName, err)
-	}
-	if resp.TotalResults == 0 {
-		return nil, fmt.Errorf("未找到电影「%s」", searchName)
-	}
-
-	var results = make([]themoviedb.SearchMovieResponse, 0, resp.TotalResults)
-	results = append(results, resp.Result...)
-
-	if resp.TotalPages > 1 {
-		for page = 2; page <= uint32(resp.TotalPages); page++ {
-			resp, err = client.SearchMovie(params)
-			if err != nil {
-				return nil, fmt.Errorf("搜索电影「%s」第 %d 页失败: %v", searchName, page, err)
+	var firstResult *themoviedb.SearchMovieResponse
+	var results iter.Seq2[*themoviedb.SearchMovieResponse, error] = func(yield func(*themoviedb.SearchMovieResponse, error) bool) {
+		var page uint32 = 1
+		var params = themoviedb.SearchMovieParams{
+			Query: searchName,
+			Page:  &page,
+		}
+		resp, err := client.SearchMovie(params)
+		if err != nil {
+			yield(nil, fmt.Errorf("搜索电影「%s」失败: %v", searchName, err))
+			return
+		}
+		if resp.TotalResults == 0 {
+			yield(nil, fmt.Errorf("未找到电影「%s」", searchName))
+			return
+		}
+		for _, result := range resp.Result {
+			if firstResult == nil {
+				firstResult = &result // 保存第一条结果
 			}
-			results = append(results, resp.Result...)
+			if !yield(&result, nil) {
+				return
+			}
+		}
+		if resp.TotalPages > 1 {
+			for page = 2; page <= uint32(resp.TotalPages); page++ {
+				resp, err = client.SearchMovie(params)
+				if err != nil {
+					if !yield(nil, fmt.Errorf("搜索电影「%s」第 %d 页失败: %v", searchName, page, err)) {
+						return
+					}
+				}
+				for _, result := range resp.Result {
+					if !yield(&result, nil) {
+						return
+					}
+				}
+			}
 		}
 	}
 
-	for _, result := range results {
+	for result, err := range results {
+		if err != nil {
+			logrus.Warning(err)
+			continue // 如果搜索失败，尝试下一个结果
+		}
+
 		if utils.FuzzyMatching(searchName, result.Title, result.OriginalTitle) {
 			logrus.Infof("匹配电影「%s」(TMDB ID: %d)", result.Title, result.ID)
-
 			return GetInfo(result.ID, meta.MediaTypeMovie)
 		}
+
 		names, err := getNames(result.ID, meta.MediaTypeMovie)
 		if err != nil {
 			logrus.Warnf("获取电影「%s(%d)」的其他名称失败: %v", result.Title, result.ID, err)
@@ -55,12 +76,11 @@ func SearchMovie(searchName string) (*schemas.MediaInfo, error) {
 		}
 		if utils.FuzzyMatching(searchName, names...) {
 			logrus.Infof("匹配电影「%s」(TMDB ID: %d) 别名", result.Title, result.ID)
-
 			return GetInfo(result.ID, meta.MediaTypeMovie)
 		}
 	}
-	logrus.Warningf("未找到电影「%s」的匹配项，返回第一项", searchName)
-	return GetInfo(results[0].ID, meta.MediaTypeMovie)
+	logrus.Warningf("未找到电影「%s」的匹配项，返回第一项: %s", searchName, firstResult.Title)
+	return GetInfo(firstResult.ID, meta.MediaTypeMovie)
 }
 
 func SearchTV(searchName string) (*schemas.MediaInfo, error) {
@@ -68,39 +88,58 @@ func SearchTV(searchName string) (*schemas.MediaInfo, error) {
 	defer lock.RUnlock()
 
 	logrus.Infof("正在搜索「%s」...", searchName)
-	var page uint32 = 1
-	var params = themoviedb.SearchTVSParams{
-		Query: searchName,
-		Page:  &page,
-	}
-
-	resp, err := client.SearchTV(params)
-	if err != nil {
-		return nil, fmt.Errorf("搜索电视剧「%s」失败: %v", searchName, err)
-	}
-	if resp.TotalResults == 0 {
-		return nil, fmt.Errorf("未找到电视剧「%s」", searchName)
-	}
-
-	var results = make([]themoviedb.SearchTVResponse, 0, resp.TotalResults)
-	results = append(results, resp.Result...)
-
-	if resp.TotalPages > 1 {
-		for page = 2; page <= uint32(resp.TotalPages); page++ {
-			resp, err = client.SearchTV(params)
-			if err != nil {
-				return nil, fmt.Errorf("搜索电视剧「%s」第 %d 页失败: %v", searchName, page, err)
+	var firstResult *themoviedb.SearchTVResponse
+	var results iter.Seq2[*themoviedb.SearchTVResponse, error] = func(yield func(*themoviedb.SearchTVResponse, error) bool) {
+		var page uint32 = 1
+		var params = themoviedb.SearchTVSParams{
+			Query: searchName,
+			Page:  &page,
+		}
+		resp, err := client.SearchTV(params)
+		if err != nil {
+			yield(nil, fmt.Errorf("搜索电视剧「%s」失败: %v", searchName, err))
+			return
+		}
+		if resp.TotalResults == 0 {
+			yield(nil, fmt.Errorf("未找到电视剧「%s」", searchName))
+			return
+		}
+		for _, result := range resp.Result {
+			if firstResult == nil {
+				firstResult = &result // 保存第一条结果
 			}
-			results = append(results, resp.Result...)
+			if !yield(&result, nil) {
+				return
+			}
+		}
+		if resp.TotalPages > 1 {
+			for page = 2; page <= uint32(resp.TotalPages); page++ {
+				resp, err = client.SearchTV(params)
+				if err != nil {
+					if !yield(nil, fmt.Errorf("搜索电视剧「%s」第 %d 页失败: %v", searchName, page, err)) {
+						return
+					}
+				}
+				for _, result := range resp.Result {
+					if !yield(&result, nil) {
+						return
+					}
+				}
+			}
 		}
 	}
 
-	for _, result := range results {
+	for result, err := range results {
+		if err != nil {
+			logrus.Warning(err)
+			continue // 如果搜索失败，尝试下一个结果
+		}
+
 		if utils.FuzzyMatching(searchName, result.Name, result.OriginalName) {
 			logrus.Infof("匹配电视剧「%s」(TMDB ID: %d)", result.Name, result.ID)
-
 			return GetInfo(result.ID, meta.MediaTypeTV)
 		}
+
 		names, err := getNames(result.ID, meta.MediaTypeTV)
 		if err != nil {
 			logrus.Warnf("获取电视剧「%s(%d)」的其他名称失败: %v", result.Name, result.ID, err)
@@ -108,12 +147,11 @@ func SearchTV(searchName string) (*schemas.MediaInfo, error) {
 		}
 		if utils.FuzzyMatching(searchName, names...) {
 			logrus.Infof("匹配电视剧「%s」(TMDB ID: %d) 别名", result.Name, result.ID)
-
 			return GetInfo(result.ID, meta.MediaTypeTV)
 		}
 	}
-	logrus.Warningf("未找到电视剧「%s」的匹配项，返回第一项", searchName)
-	return GetInfo(results[0].ID, meta.MediaTypeTV)
+	logrus.Warningf("未找到电视剧「%s」的匹配项，返回第一项: %s", searchName, firstResult.Name)
+	return GetInfo(firstResult.ID, meta.MediaTypeTV)
 }
 
 func SearchMulti(searchName string) (*schemas.MediaInfo, error) {
@@ -121,30 +159,53 @@ func SearchMulti(searchName string) (*schemas.MediaInfo, error) {
 	defer lock.RUnlock()
 
 	logrus.Infof("正在综合搜索「%s」...", searchName)
-	var page uint32 = 1
-	var params = themoviedb.SearchMultiParams{
-		Query: searchName,
-		Page:  &page,
-	}
-	resp, err := client.SearchMulti(params)
-	if err != nil {
-		return nil, fmt.Errorf("综合搜索「%s」失败: %v", searchName, err)
-	}
-	if resp.TotalResults == 0 {
-		return nil, fmt.Errorf("未找到综合搜索结果「%s」", searchName)
-	}
-	var results = make([]themoviedb.SearchMultiResponse, 0, resp.TotalResults)
-	results = append(results, resp.Result...)
-	if resp.TotalPages > 1 {
-		for page = 2; page <= uint32(resp.TotalPages); page++ {
-			resp, err = client.SearchMulti(params)
-			if err != nil {
-				return nil, fmt.Errorf("综合搜索「%s」第 %d 页失败: %v", searchName, page, err)
+	var firstResult *themoviedb.SearchMultiResponse
+	var results iter.Seq2[*themoviedb.SearchMultiResponse, error] = func(yield func(*themoviedb.SearchMultiResponse, error) bool) {
+		var page uint32 = 1
+		var params = themoviedb.SearchMultiParams{
+			Query: searchName,
+			Page:  &page,
+		}
+		resp, err := client.SearchMulti(params)
+		if err != nil {
+			yield(nil, fmt.Errorf("综合搜索「%s」失败: %v", searchName, err))
+			return
+		}
+		if resp.TotalResults == 0 {
+			yield(nil, fmt.Errorf("未找到综合搜索结果「%s」", searchName))
+			return
+		}
+		for _, res := range resp.Result {
+			if firstResult == nil {
+				firstResult = &res // 保存第一条结果
 			}
-			results = append(results, resp.Result...)
+			if !yield(&res, nil) {
+				return
+			}
+		}
+		if resp.TotalPages > 1 {
+			for page = 2; page <= uint32(resp.TotalPages); page++ {
+				resp, err = client.SearchMulti(params)
+				if err != nil {
+					if !yield(nil, fmt.Errorf("综合搜索「%s」第 %d 页失败: %v", searchName, page, err)) {
+						return
+					}
+				}
+				for _, res := range resp.Result {
+					if !yield(&res, nil) {
+						return
+					}
+				}
+			}
 		}
 	}
-	for _, result := range results {
+
+	for result, err := range results {
+		if err != nil {
+			logrus.Warning(err)
+			continue // 如果搜索失败，尝试下一个结果
+		}
+
 		mediaType := parseType(result.MediaType)
 		if mediaType == meta.MediaTypeUnknown {
 			logrus.Warningf("综合搜索结果「%s」的媒体类型(%s)未知，跳过", result.Title, result.MediaType)
@@ -155,6 +216,7 @@ func SearchMulti(searchName string) (*schemas.MediaInfo, error) {
 			logrus.Infof("匹配综合搜索结果「%s」(Type: %s TMDB ID: %d)", result.Title, mediaType, result.ID)
 			return GetInfo(result.ID, mediaType)
 		}
+
 		names, err := getNames(result.ID, mediaType)
 		if err != nil {
 			logrus.Warnf("获取综合搜索结果「%s(Type: %s TMDB ID: %d)」的其他名称失败: %v", result.Title, mediaType, result.ID, err)
@@ -166,10 +228,11 @@ func SearchMulti(searchName string) (*schemas.MediaInfo, error) {
 			return GetInfo(result.ID, mediaType)
 		}
 	}
-	result := results[0]
-	mediaType := parseType(result.MediaType)
+
+	logrus.Warningf("未找到综合搜索「%s」的匹配项，返回第一项: %s", searchName, firstResult.Title)
+	mediaType := parseType(firstResult.MediaType)
 	if mediaType == meta.MediaTypeUnknown {
-		logrus.Warningf("综合搜索结果「%s」的媒体类型(%s)未知", result.Title, result.MediaType)
+		logrus.Warningf("综合搜索结果「%s」的媒体类型(%s)未知", firstResult.Title, firstResult.MediaType)
 	}
-	return GetInfo(result.ID, mediaType)
+	return GetInfo(firstResult.ID, mediaType)
 }
