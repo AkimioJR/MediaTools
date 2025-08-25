410311f8026b2e89fc5ec32db12c72e7a06daec8
Akimio521 <akimio2333@gmail.com>
2025-08-25 11:12:50 +0800

    feat(task_controller): 添加 GetTransferTask 和 IterTransferTasks 方法以获取和遍历任务

--- internal/controller/task_controller/transfer.go
+++ internal/controller/task_controller/transfer.go
@@ -6,6 +6,14 @@ func SubmitTransferTask(name string, fn task.TaskFunc) *task.Task {
 	return transferTaskQueue.SubmitTask(name, fn)
 }
 
+func GetTransferTask(id string) (*task.Task, error) {
+	return transferTaskQueue.GetTask(id)
+}
+
 func CancelTransferTask(id string) (*task.Task, error) {
 	return transferTaskQueue.CancelTask(id)
+}
+
+func IterTransferTasks(yield func(t *task.Task) bool) {
+	transferTaskQueue.IterTasks(yield)
 }
