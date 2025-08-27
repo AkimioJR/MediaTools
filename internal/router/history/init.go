44b8f229226920ab115aec6a6d2c71ec2eb65e16
Akimio521 <akimio2333@gmail.com>
2025-08-27 16:44:25 +0800

    refactor(history): 修改媒体历史记录查询路由

--- internal/router/history/init.go
+++ internal/router/history/init.go
@@ -6,6 +6,6 @@ import "github.com/gin-gonic/gin"
 func RegisterHistoryRouter(historyRouter *gin.RouterGroup) {
 	mediaHistoryRouter := historyRouter.Group("/media") // 媒体历史记录相关路由
 	{
-		mediaHistoryRouter.GET("/transfer", QueryMediaTransferHistory) // 查询媒体转移历史记录
+		mediaHistoryRouter.GET("", QueryMediaTransferHistory) // 查询媒体转移历史记录
 	}
 }
