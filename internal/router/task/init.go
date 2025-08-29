864fe3842868595d957daacc0c33c6da998e5b8b
Akimio521 <akimio2333@gmail.com>
2025-08-29 12:37:48 +0800

    fix(RegisterTaskRouter): 修复转移任务列表接口路径，去掉多余的斜杠

--- internal/router/task/init.go
+++ internal/router/task/init.go
@@ -6,7 +6,7 @@ func RegisterTaskRouter(taskRouter *gin.RouterGroup) {
 
 	transferRouter := taskRouter.Group("/transfer") // 媒体转移任务相关接口
 	{
-		transferRouter.GET("/", GetAllTransferTasks)      // 查询转移任务列表
+		transferRouter.GET("", GetAllTransferTasks)       // 查询转移任务列表
 		transferRouter.GET("/:id", GetTransferTask)       // 获取转移任务状态
 		transferRouter.DELETE("/:id", CancelTransferTask) // 取消转移任务
 	}
