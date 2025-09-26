5b18d41c1510a98275787e8c0bcb8774bc4c6ce5
Akimio521 <akimio2333@gmail.com>
2025-09-26 21:24:06 +0800

    build: 新增 Linux 脚本以更新软件包并安装必要的开发库

---
+++ scripts/linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e  # 如果任何命令失败就退出
+
+echo "正在更新软件包列表..."
+apt-get update
+
+echo "正在安装必要的开发库..."
+apt-get install -y \
+    libgtk-3-dev \
+    libwebkit2gtk-4.1-dev \
+    pkg-config
