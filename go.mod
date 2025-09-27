fcf1a1877f2f9ab43650c1f49be789e4fd8177e0
Akimio521 <akimio2333@gmail.com>
2025-09-27 22:34:32 +0800

    feat: 新增系统托盘

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module MediaTools
 go 1.24.1
 
 require (
+	fyne.io/systray v1.11.0
 	github.com/allegro/bigcache v1.2.1
 	github.com/gin-gonic/gin v1.10.1
 	github.com/google/uuid v1.6.0
@@ -37,6 +38,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
--- internal/app/app_desktop.go
+++ internal/app/app_desktop.go
@@ -4,32 +4,86 @@ package app
 
 import (
 	"fmt"
-	"os"
 
+	"fyne.io/systray"
 	"github.com/sirupsen/logrus"
 	webview "github.com/webview/webview_go"
 )
 
 const AppName = "MediaTools"
 
-var SupportDesktopMode = true
+var (
+	SupportDesktopMode = true
+	globalView         = webview.New(false)
+)
 
-func openWindows() {
-	w := webview.New(false)
-	defer w.Destroy()
-	w.SetSize(800, 600, webview.HintNone)
-	w.SetTitle(AppName)
-	w.Navigate(fmt.Sprintf("http://localhost:%d", port))
-	w.Run()
+func showWindow() {
+	logrus.Debug("显示窗口")
+	globalView.Dispatch(func() {
+		globalView.Navigate(fmt.Sprintf("http://localhost:%d", port))
+		globalView.SetSize(800, 600, webview.HintNone)
+	})
+}
+
+func hideWindow() {
+	logrus.Debug("隐藏窗口")
+	globalView.Dispatch(func() {
+		globalView.SetHtml("")
+		globalView.SetSize(0, 0, webview.HintNone)
+	})
+}
+
+func onReady() {
+	systray.SetTitle(AppName)
+	systray.SetTooltip(AppName + " - 工具栏")
+	quitItem := systray.AddMenuItem("退出", "退出应用程序")
+	showWindowsItem := systray.AddMenuItem("显示窗口", "显示应用程序窗口")
+	hideWindowsItem := systray.AddMenuItem("隐藏窗口", "隐藏应用程序窗口")
+	go func() {
+		for {
+			select {
+			case <-showWindowsItem.ClickedCh:
+				showWindow()
+			case <-hideWindowsItem.ClickedCh:
+				hideWindow()
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+			}
+		}
+	}()
+	logrus.Debug("系统托盘启动成功")
+}
+
+func onExit(quitFlag *bool) func() {
+	return func() {
+		logrus.Info("退出应用程序中...")
+		globalView.Terminate()
+		*quitFlag = true
+	}
 }
 
 func runDesktop() {
 	logrus.Infof("桌面模式启动中，端口: %d", port)
 	go func() {
 		runServer()
-		os.Exit(0)
+		systray.Quit()
 	}()
-	openWindows()
+
+	quitFlag := false
+	startFn, endFn := systray.RunWithExternalLoop(onReady, onExit(&quitFlag))
+	startFn()
+	defer endFn()
+
+	globalView.SetTitle(AppName)
+	defer globalView.Destroy()
+	for !quitFlag {
+		showWindow()
+		logrus.Debug("webview 运行中...")
+		globalView.Run()
+		logrus.Debug("webview 运行结束")
+		systray.Quit()
+	}
+
 }
 
 func Run() {
