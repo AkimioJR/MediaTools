bc595cdb8a2f70732a4556ff20dbbc69eff95fdf
Akimio521 <akimio2333@gmail.com>
2025-09-27 23:06:55 +0800

    feat: 重构窗口显示逻辑，优化托盘菜单状态更新

--- internal/app/app_desktop.go
+++ internal/app/app_desktop.go
@@ -14,23 +14,48 @@ const AppName = "MediaTools"
 
 var (
 	SupportDesktopMode = true
-	globalView         = webview.New(false)
+	globalView         webview.WebView
+	isWindowVisible    = false
+	showWindowChan     = make(chan bool, 1)
+	updateTrayMenuChan = make(chan bool, 1) // 新增：用于更新托盘菜单状态
+	quitFlag           = false
 )
 
-func showWindow() {
-	logrus.Debug("显示窗口")
-	globalView.Dispatch(func() {
-		globalView.Navigate(fmt.Sprintf("http://localhost:%d", port))
-		globalView.SetSize(800, 600, webview.HintNone)
-	})
+func createWebView() {
+	if globalView != nil {
+		globalView.Destroy()
+	}
+	globalView = webview.New(false)
+	globalView.SetTitle(AppName)
 }
 
-func hideWindow() {
-	logrus.Debug("隐藏窗口")
-	globalView.Dispatch(func() {
-		globalView.SetHtml("")
-		globalView.SetSize(0, 0, webview.HintNone)
-	})
+func showWindow() {
+	if !isWindowVisible {
+		logrus.Debug("显示窗口")
+		if globalView == nil {
+			createWebView()
+		}
+
+		// 使用 Dispatch 确保在主线程中执行 UI 操作
+		globalView.Dispatch(func() {
+			globalView.SetSize(800, 600, webview.HintNone)
+			globalView.Navigate(fmt.Sprintf("http://localhost:%d", port))
+		})
+
+		isWindowVisible = true
+
+		// 通知等待线程窗口已显示
+		select {
+		case showWindowChan <- true:
+		default:
+		}
+	}
+}
+
+func waitForWindowShow() {
+	logrus.Debug("等待窗口显示信号...")
+	<-showWindowChan
+	logrus.Debug("收到窗口显示信号")
 }
 
 func onReady() {
@@ -46,20 +71,37 @@ func onReady() {
 	quitItem := systray.AddMenuItem("退出", "退出应用程序")
 
 	go func() {
-		showWindows := true
 		for {
 			select {
 			case <-switchWindowStatusItem.ClickedCh:
-				if showWindows {
-					hideWindow()
+				if isWindowVisible {
+					logrus.Debug("用户从托盘隐藏窗口")
+					isWindowVisible = false
 					switchWindowStatusItem.SetTitle(showTitle)
 					switchWindowStatusItem.SetTooltip(showTip)
+					// 安全地终止当前 webview
+					if globalView != nil {
+						globalView.Dispatch(func() {
+							globalView.Terminate()
+						})
+					}
 				} else {
+					logrus.Debug("用户从托盘显示窗口")
 					showWindow()
 					switchWindowStatusItem.SetTitle(hideTitle)
 					switchWindowStatusItem.SetTooltip(hideTip)
 				}
-				showWindows = !showWindows
+
+			case <-updateTrayMenuChan:
+				// 根据当前窗口状态更新托盘菜单
+				if isWindowVisible {
+					switchWindowStatusItem.SetTitle(hideTitle)
+					switchWindowStatusItem.SetTooltip(hideTip)
+				} else {
+					switchWindowStatusItem.SetTitle(showTitle)
+					switchWindowStatusItem.SetTooltip(showTip)
+				}
+				logrus.Debug("已更新系统托盘菜单状态")
 
 			case <-quitItem.ClickedCh:
 				systray.Quit()
@@ -69,11 +111,16 @@ func onReady() {
 	logrus.Debug("系统托盘启动成功")
 }
 
-func onExit(quitFlag *bool) func() {
+func onExit() func() {
 	return func() {
 		logrus.Info("退出应用程序中...")
-		globalView.Terminate()
-		*quitFlag = true
+		quitFlag = true
+		if globalView != nil {
+			// 确保在主线程中终止 webview
+			globalView.Dispatch(func() {
+				globalView.Terminate()
+			})
+		}
 	}
 }
 
@@ -84,21 +131,58 @@ func runDesktop() {
 		systray.Quit()
 	}()
 
-	quitFlag := false
-	startFn, endFn := systray.RunWithExternalLoop(onReady, onExit(&quitFlag))
+	startFn, endFn := systray.RunWithExternalLoop(onReady, onExit())
 	startFn()
 	defer endFn()
 
-	globalView.SetTitle(AppName)
-	defer globalView.Destroy()
-	for !quitFlag {
-		showWindow()
-		logrus.Debug("webview 运行中...")
-		globalView.Run()
-		logrus.Debug("webview 运行结束")
-		systray.Quit()
-	}
+	// 标记是否首次启动
+	firstLaunch := true
 
+	// 主循环：持续运行直到用户明确选择退出
+	for !quitFlag {
+		// 只在首次启动或用户主动请求时显示窗口
+		if firstLaunch {
+			// 首次启动，创建并显示窗口
+			createWebView()
+			defer globalView.Destroy()
+			showWindow()
+			firstLaunch = false
+
+			// 确保托盘菜单状态正确
+			select {
+			case updateTrayMenuChan <- true:
+			default:
+			}
+		} else {
+			// 等待用户通过系统托盘请求显示窗口
+			logrus.Debug("等待用户通过系统托盘重新打开窗口")
+			waitForWindowShow()
+
+			if !quitFlag {
+				// 创建新的 webview 实例
+				createWebView()
+				defer globalView.Destroy()
+				// showWindow() 已经在托盘处理中被调用
+			}
+		}
+
+		// 如果没有退出，运行 webview
+		if !quitFlag {
+			logrus.Debug("webview 运行中...")
+			globalView.Run()
+			logrus.Debug("webview 运行结束")
+
+			// 当 webview.Run() 退出时（用户关闭窗口），更新状态
+			isWindowVisible = false
+
+			// 通知系统托盘更新菜单状态
+			select {
+			case updateTrayMenuChan <- true:
+			default:
+			}
+		}
+	}
+	logrus.Info("程序正常退出")
 }
 
 func Run() {
