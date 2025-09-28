9970da66029b65780223c9046e0e1a81641d6e71
Akimio521 <akimio2333@gmail.com>
2025-09-28 14:37:14 +0800

    fix: 修复服务器模式下 Run 函数问题

--- internal/app/app_server.go
+++ internal/app/app_server.go
@@ -2,21 +2,10 @@
 
 package app
 
-import "github.com/sirupsen/logrus"
-
 var (
 	SupportDesktopMode = false
 )
 
 func Run() {
-	serverCh := runServer()
-	
-	select {
-	case err := <-serverCh:
-		if err != nil {
-			logrus.Errorf("应用程序运行中发生错误: %v", err)
-		}
-	case sig := <-sysCh:
-		logrus.Infof("收到系统信号: %v, 退出应用程序", sig)
-	}
+	runServer()
 }
