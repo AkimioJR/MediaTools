03c93e81d87f8f5fb643cc84013da6c258856eaa
Akimio521 <akimio2333@gmail.com>
2025-09-28 09:51:57 +0800

    refactor: 拆分桌面模式下 ui 组件

--- internal/app/app_desktop.go
+++ internal/app/app_desktop.go
@@ -7,111 +7,9 @@ import (
 
 	"fyne.io/systray"
 	"github.com/sirupsen/logrus"
-	webview "github.com/webview/webview_go"
 )
 
-const AppName = "MediaTools"
-
-var (
-	SupportDesktopMode = true
-	globalView         webview.WebView
-	isWindowVisible    = false
-	showWindowChan     = make(chan struct{}, 1)
-	updateTrayMenuChan = make(chan struct{}, 5) // 新增：用于更新托盘菜单状态
-	quitFlag           = false
-)
-
-func createWebView() {
-	if globalView != nil {
-		globalView.Destroy()
-	}
-	globalView = webview.New(false)
-	globalView.SetTitle(AppName)
-}
-
-func showWindow() {
-	if !isWindowVisible {
-		logrus.Debug("显示窗口")
-		if globalView == nil {
-			createWebView()
-		}
-
-		// 使用 Dispatch 确保在主线程中执行 UI 操作
-		globalView.Dispatch(func() {
-			globalView.SetSize(800, 600, webview.HintNone)
-			globalView.Navigate(fmt.Sprintf("http://localhost:%d", port))
-		})
-
-		isWindowVisible = true
-
-		// 通知等待线程窗口已显示
-		select {
-		case showWindowChan <- struct{}{}:
-		default:
-		}
-	}
-}
-
-func onReady() {
-	const (
-		showTitle = "显示窗口"
-		showTip   = "显示应用程序窗口"
-		hideTitle = "隐藏窗口"
-		hideTip   = "隐藏应用程序窗口"
-	)
-	systray.SetTitle(AppName)
-	systray.SetTooltip(AppName + " - 工具栏")
-	switchWindowStatusItem := systray.AddMenuItem(hideTitle, hideTip)
-	quitItem := systray.AddMenuItem("退出", "退出应用程序")
-
-	go func() {
-		for {
-			select {
-			case <-switchWindowStatusItem.ClickedCh:
-				if isWindowVisible {
-					logrus.Debug("用户从托盘隐藏窗口")
-					isWindowVisible = false
-					// 安全地终止当前 webview
-					if globalView != nil {
-						globalView.Dispatch(func() {
-							globalView.Terminate()
-						})
-					}
-				} else {
-					logrus.Debug("用户从托盘显示窗口")
-					showWindow()
-				}
-				updateTrayMenuChan <- struct{}{}
-
-			case <-updateTrayMenuChan:
-				// 根据当前窗口状态更新托盘菜单
-				if isWindowVisible {
-					switchWindowStatusItem.SetTitle(hideTitle)
-					switchWindowStatusItem.SetTooltip(hideTip)
-				} else {
-					switchWindowStatusItem.SetTitle(showTitle)
-					switchWindowStatusItem.SetTooltip(showTip)
-				}
-				logrus.Debug("已更新系统托盘菜单状态")
-
-			case <-quitItem.ClickedCh:
-				systray.Quit()
-			}
-		}
-	}()
-	logrus.Debug("系统托盘启动成功")
-}
-
-func onExit() {
-	logrus.Info("退出应用程序中...")
-	quitFlag = true
-	if globalView != nil {
-		// 确保在主线程中终止 webview
-		globalView.Dispatch(func() {
-			globalView.Terminate()
-		})
-	}
-}
+var SupportDesktopMode = true
 
 func runDesktop() {
 	logrus.Infof("桌面模式启动中，端口: %d", port)
@@ -119,36 +17,8 @@ func runDesktop() {
 		runServer()
 		systray.Quit()
 	}()
-
-	startFn, endFn := systray.RunWithExternalLoop(onReady, onExit)
-	startFn()
-	defer endFn()
-
-	firstLaunch := true
-	for !quitFlag { // 只在首次启动或用户主动请求时显示窗口
-		if firstLaunch { // 首次启动，创建并显示窗口
-			createWebView()
-			defer globalView.Destroy()
-			showWindow()
-			firstLaunch = false
-		} else {
-			logrus.Debug("等待用户通过系统托盘重新打开窗口")
-			<-showWindowChan // 等待用户通过系统托盘请求显示窗口
-
-			if !quitFlag {
-				createWebView() // 创建新的 webview 实例
-				defer globalView.Destroy()
-			}
-		}
-
-		if !quitFlag { // 如果没有退出，运行 webview
-			logrus.Debug("webview 运行中...")
-			globalView.Run()
-			logrus.Debug("webview 运行结束")
-			isWindowVisible = false          // 当 webview.Run() 退出时（用户关闭窗口），更新状态
-			updateTrayMenuChan <- struct{}{} // 通知系统托盘更新菜单状态
-		}
-	}
+	ui := NewUIManager(fmt.Sprintf("http://localhost:%d", port))
+	ui.Run()
 	logrus.Info("程序正常退出")
 }
 
---
+++ internal/app/ui.go
@@ -0,0 +1,156 @@
+//go:build !onlyServer
+
+package app
+
+import (
+	"fyne.io/systray"
+	"github.com/sirupsen/logrus"
+	webview "github.com/webview/webview_go"
+)
+
+const AppName = "MediaTools"
+
+type UIManager struct {
+	url                string
+	view               webview.WebView
+	isVisible          bool
+	showVisibleChan    chan struct{}
+	updateTrayMenuChan chan struct{}
+	quitFlag           bool
+}
+
+func NewUIManager(url string) *UIManager {
+	return &UIManager{
+		url:                url,
+		view:               webview.New(false),
+		isVisible:          false,
+		showVisibleChan:    make(chan struct{}, 1),
+		updateTrayMenuChan: make(chan struct{}, 5),
+		quitFlag:           false,
+	}
+}
+
+func (ui *UIManager) createWebView() {
+	if ui.view != nil {
+		ui.view.Destroy()
+		ui.view = nil
+	}
+	ui.view = webview.New(false)
+	ui.view.SetTitle(AppName)
+}
+
+func (ui *UIManager) showWindow() {
+	if !ui.isVisible {
+		logrus.Debug("显示窗口")
+		if ui.view == nil {
+			ui.createWebView()
+		}
+
+		// 使用 Dispatch 确保在主线程中执行 UI 操作
+		ui.view.Dispatch(func() {
+			ui.view.SetSize(800, 600, webview.HintNone)
+			ui.view.Navigate(ui.url)
+		})
+
+		ui.isVisible = true
+
+		// 通知等待线程窗口已显示
+		select {
+		case ui.showVisibleChan <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (ui *UIManager) onReady() {
+	const (
+		showTitle = "显示窗口"
+		showTip   = "显示应用程序窗口"
+		hideTitle = "隐藏窗口"
+		hideTip   = "隐藏应用程序窗口"
+	)
+	systray.SetTitle(AppName)
+	systray.SetTooltip(AppName + " - 工具栏")
+	switchWindowStatusItem := systray.AddMenuItem(hideTitle, hideTip)
+	quitItem := systray.AddMenuItem("退出", "退出应用程序")
+
+	go func() {
+		for {
+			select {
+			case <-switchWindowStatusItem.ClickedCh:
+				if ui.isVisible {
+					logrus.Debug("用户从托盘隐藏窗口")
+					ui.isVisible = false
+					// 安全地终止当前 webview
+					if ui.view != nil {
+						ui.view.Dispatch(func() {
+							ui.view.Terminate()
+						})
+					}
+				} else {
+					logrus.Debug("用户从托盘显示窗口")
+					ui.showWindow()
+				}
+				ui.updateTrayMenuChan <- struct{}{}
+
+			case <-ui.updateTrayMenuChan:
+				// 根据当前窗口状态更新托盘菜单
+				if ui.isVisible {
+					switchWindowStatusItem.SetTitle(hideTitle)
+					switchWindowStatusItem.SetTooltip(hideTip)
+				} else {
+					switchWindowStatusItem.SetTitle(showTitle)
+					switchWindowStatusItem.SetTooltip(showTip)
+				}
+				logrus.Debug("已更新系统托盘菜单状态")
+
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+			}
+		}
+	}()
+	logrus.Debug("系统托盘启动成功")
+}
+
+func (ui *UIManager) onExit() {
+	logrus.Info("退出应用程序中...")
+	ui.quitFlag = true
+	if ui.view != nil {
+		// 确保在主线程中终止 webview
+		ui.view.Dispatch(func() {
+			ui.view.Terminate()
+		})
+	}
+}
+
+func (ui *UIManager) Run() {
+	startFn, endFn := systray.RunWithExternalLoop(ui.onReady, ui.onExit)
+	startFn()
+	defer endFn()
+
+	firstLaunch := true
+	for !ui.quitFlag { // 只在首次启动或用户主动请求时显示窗口
+		if firstLaunch { // 首次启动，创建并显示窗口
+			ui.createWebView()
+			defer ui.view.Destroy()
+			ui.showWindow()
+			firstLaunch = false
+		} else {
+			logrus.Debug("等待用户通过系统托盘重新打开窗口")
+			<-ui.showVisibleChan // 等待用户通过系统托盘请求显示窗口
+
+			if !ui.quitFlag {
+				ui.createWebView() // 创建新的 webview 实例
+				defer ui.view.Destroy()
+			}
+		}
+
+		if !ui.quitFlag { // 如果没有退出，运行 webview
+			logrus.Debug("webview 运行中...")
+			ui.view.Run()
+			logrus.Debug("webview 运行结束")
+			ui.isVisible = false                // 当 webview.Run() 退出时（用户关闭窗口），更新状态
+			ui.updateTrayMenuChan <- struct{}{} // 通知系统托盘更新菜单状态
+		}
+	}
+}
