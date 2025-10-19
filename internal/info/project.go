62d1ee6909beec1f5acf2083e097b952e668e93b
AkimioJR <akimio.jr@gmail.com>
2025-10-19 16:20:53 +0800

    update: 添加项目作者、邮箱和项目地址

--- internal/info/project.go
+++ internal/info/project.go
@@ -1,9 +1,12 @@
 package info
 
 const (
-	ProjectName = "MediaTools"
-	Copyright   = "Copyright © 2025 AKimioJR(akimio.jr@gmail.com)"
-	ProjectLogo = `
+	ProjectName   = "MediaTools"
+	ProjectAuthor = "AKimioJR"
+	ProjectEmail  = "akimio.jr@gmail.com"
+	ProjectURL    = "https://github.com/" + ProjectAuthor + "/" + ProjectName
+	Copyright     = "Copyright © 2025" + " " + ProjectAuthor + " <" + ProjectEmail + ">. All rights reserved."
+	ProjectLogo   = `
 ███╗   ███╗███████╗██████╗ ██╗ █████╗ ████████╗ ██████╗  ██████╗ ██╗     ███████╗
 ████╗ ████║██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
 ██╔████╔██║█████╗  ██║  ██║██║███████║   ██║   ██║   ██║██║   ██║██║     ███████╗
